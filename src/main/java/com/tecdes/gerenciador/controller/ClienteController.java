279767acb4da7dc0ffe26bc996df60b90025cabb
Caíque Oliveira <caique_oliveira@estudante.sc.senai.br>
2025-11-25 19:26:43 -0300

    Implementação parcial ClienteController

---
+++ src/main/java/com/tecdes/gerenciador/controller/ClienteController.java
@@ -0,0 +1,11 @@
+package com.tecdes.gerenciador.controller;
+
+import com.tecdes.gerenciador.model.entity.Cliente;
+
+public class ClienteController {
+    private Cliente model;
+
+    public ClienteController(Cliente model){
+        this.model = model;
+    }
+}
