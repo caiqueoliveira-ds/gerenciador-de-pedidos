b875be2d9e0fe7f59eb620f259b289ff577cc428
Caíque Oliveira <caique_oliveira@estudante.sc.senai.br>
2025-11-25 19:39:34 -0300

    Implementação parcial ProdutoForm

--- src/main/java/com/tecdes/gerenciador/view/ProdutoForm.java
+++ src/main/java/com/tecdes/gerenciador/view/ProdutoForm.java
@@ -1,5 +1,7 @@
 package com.tecdes.gerenciador.view;
 
-public class ProdutoForm {
+import com.tecdes.gerenciador.controller.ProdutoController;
 
+public class ProdutoForm {
+    private ProdutoController controller;
 }
