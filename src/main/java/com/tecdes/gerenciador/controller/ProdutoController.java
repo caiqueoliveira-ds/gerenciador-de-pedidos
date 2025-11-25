55ff2731b98dcf77608e655232d7957c15bf2c30
Caíque Oliveira <caique_oliveira@estudante.sc.senai.br>
2025-11-25 19:37:48 -0300

    Implementando parcialmente o ProdutoController

---
+++ src/main/java/com/tecdes/gerenciador/controller/ProdutoController.java
@@ -0,0 +1,11 @@
+package com.tecdes.gerenciador.controller;
+
+import com.tecdes.gerenciador.model.entity.Produto;
+
+public class ProdutoController {
+    private Produto model;
+
+    public ProdutoController(Produto model){
+        this.model = model;
+    }
+}
